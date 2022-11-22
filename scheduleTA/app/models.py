0ab337343a2e8b0d220ffa68f2f73026f2220e46
johnpiapian <johnpiapian@gmail.com>
2022-11-22 16:36:43 -0600

    Add general model classes

--- scheduleTA/app/models.py
+++ scheduleTA/app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    role = models.CharField(max_length=1, blank=False)
+    username = models.CharField(max_length=100, blank=False)
+    password = models.CharField(max_length=100, blank=False)
+
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False, unique=True)
+    credit = models.IntegerField(blank=False)
+
+
+class Section(models.Model):
+    id = models.AutoField(primary_key=True)
+    course_id = models.ForeignKey(Course, on_delete=models.PROTECT)
+    teacher_id = models.ForeignKey(User, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100, blank=False)
+    room_number = models.IntegerField(blank=False)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
---
+++ scheduleTA/classes/UserClass.py
@@ -0,0 +1,5 @@
+from app.models import User
+
+class UserClass:
+    def __int__(self):
+        pass
