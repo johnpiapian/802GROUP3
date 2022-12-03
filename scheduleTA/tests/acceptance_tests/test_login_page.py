5f4bf1e4cb49610042044f9225629487f8eae43c
Colin Young <ColinMatthewsYoung@gmail.com>
2022-12-03 10:24:31 -0600

    Fixed login acceptacne to take error message used.

--- scheduleTA/tests/acceptance_tests/test_login_page.py
+++ scheduleTA/tests/acceptance_tests/test_login_page.py
@@ -7,18 +7,18 @@ class Login(test_SetUp.dbSetup):
     def test_incorrectPassword(self):
         for i in self.userList:
             resp = self.mockUser.post("/", {"name": i.name, "password": "a"}, follow=True)
-            self.assertEqual(resp.context["message"],"Password is incorrect")
+            self.assertEqual(resp.context["message"],"Login Error: Invalid password, try again.")
 
     def test_noPassword(self):
         for i in self.userList:
             resp = self.mockUser.post("/", {"name": i.name, "password": ""}, follow=True)
-            self.assertEqual(resp.context["message"],"Password is incorrect")
+            self.assertEqual(resp.context["message"],"Login Error: Invalid password, try again.")
 
     def test_noSuchUser(self):
 
         for i in self.userList:
             resp = self.mockUser.post("/", {"name": "a", "password": i.password}, follow=True)
-            self.assertEqual(resp.context["message"], 'UserName is incorrect')
+            self.assertEqual(resp.context["message"], 'Login Error: Invalid username, try again.')
 
     def test_goodLoggin(self):
         for i in self.userList:
