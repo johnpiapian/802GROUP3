99e8517334c1c8ed843c68eab8355a2a96d8e3a1
Colin Young <ColinMatthewsYoung@gmail.com>
2022-12-03 11:07:26 -0600

    Updated tests for Password correct/ get Role

--- scheduleTA/tests/unit_tests/test_User.py
+++ scheduleTA/tests/unit_tests/test_User.py
@@ -14,17 +14,32 @@ class UserUnitTests(test_SetUp.dbSetup):
     def test_userExists_02(self):
         self.assertEqual(False, UserClass.UserClass.userExists(self, 'A'))
     def test_userExists_03(self):
-        self.assertEqual(False, UserClass.UserClass.userExists(self, 1234))
+        self.assertEqual(False, UserClass.UserClass.userExists(self, None))
     def test_getUser_00(self):
         for i in self.userList:
             self.assertEqual(i, UserClass.UserClass.getUser(self, i.name))
             self.assertEqual(i, UserClass.UserClass.getUser(self, i.name.upper()))
+
+    def test_passwordCorrect_00(self):
+        self.assertEqual(True, UserClass.UserClass.passwordCorrect(self,self.Colin,'1234'))
+    def test_passwordCorrect_01(self):
+        self.assertEqual(False, UserClass.UserClass.passwordCorrect(self,self.Admin,'1234'))
+    def test_passwordCorrect_02(self):
+        self.assertEqual(True, UserClass.UserClass.passwordCorrect(self,self.Colin,1234))
+    def test_passwordCorrect_03(self):
+        self.assertEqual(False, UserClass.UserClass.passwordCorrect(self,self.Admin,'pAsSwOrD'))
+    def test_passwordCorrect_04(self):
+        self.assertEqual(False, UserClass.UserClass.passwordCorrect(self, self.Admin, ''))
+    def test_passwordCorrect_05(self):
+        self.assertEqual(False, UserClass.UserClass.passwordCorrect(self, None, '1234'))
+
+
     def test_getUser_01(self):
         self.assertEqual(None, UserClass.UserClass.getUser(self, ''))
     def test_getUser_02(self):
         self.assertEqual(None, UserClass.UserClass.getUser(self, 'A'))
     def test_getUser_03(self):
-        self.assertEqual(None, UserClass.UserClass.getUser(self, 1234))
+        self.assertEqual(None, UserClass.UserClass.getUser(self, None))
 
     def test_addUser_00(self):
         self.assertEqual(True, UserClass.UserClass.addUser(self, ['A','John','Test']))
@@ -47,6 +62,16 @@ class UserUnitTests(test_SetUp.dbSetup):
         self.assertEqual(False, UserClass.UserClass.updateUser(self, ['','Colin','newPassword']))
     def test_updateUser_05(self):
         self.assertEqual(False, UserClass.UserClass.updateUser(self, ['A','asdjksdjkdsf','newPassword']))
+
+    def test_getRole_00(self):
+        self.assertEqual('A', UserClass.UserClass.getRole(self,self.Colin.name))
+    def test_getRole_01(self):
+        self.assertEqual(None, UserClass.UserClass.getRole(self,'Randy'))
+    def test_getRole_02(self):
+        self.assertEqual(None, UserClass.UserClass.getRole(self,''))
+    def test_getRole_03(self):
+        self.assertEqual(None, UserClass.UserClass.getRole(self,None))
+
 
 
     def test_deleteUser_00(self):
