8ad81537acc316ed5c8f061b6342da9b265e05f0
Colin Young <ColinMatthewsYoung@gmail.com>
2022-12-05 21:11:52 -0600

    Fixed tests for create user page. Everything working except nonadmin test

--- scheduleTA/tests/acceptance_tests/test_create_user_page.py
+++ scheduleTA/tests/acceptance_tests/test_create_user_page.py
@@ -10,25 +10,25 @@ class newUser(test_SetUp.dbSetup):
         session = self.client.session
         session['roll'] = "TA"
         session.save()
-        resp = self.mockUser.get("/createUser/", follow=True)
-        self.assertTemplateUsed(resp.url, 'homepage_0/')
+        resp = self.mockUser.get("/create_user/", follow=True)
+        self.assertEqual(resp,[('/homepage_0/',302)])
 
     def test_userNameTaken(self):
         for i in self.userList:
-            resp = self.mockUser.post("/createUser/", {"name": i.name, "password": "pass", "role": "A"}, follow=True)
-            self.assertEqual(resp.context["message"], "User already exists")
+            resp = self.mockUser.post("/create_user/", {"name": i.name, "password": "pass", "role": "A"}, follow=True)
+            self.assertEqual(resp.context["message"], "ERROR: Username already exists in database, try again.")
 
     def test_noPassword(self):
-        resp = self.mockUser.post("/createUser/", {"name": "test", "password": "", "role": "A"}, follow=True)
+        resp = self.mockUser.post("/create_user/", {"name": "test", "password": "", "role": "A"}, follow=True)
         self.assertEqual(resp.context["message"], "needs password")
 
     def test_noUserName(self):
-        resp = self.mockUser.post("/createUser/", {"name": "", "password": "1234", "role": "A"}, follow=True)
+        resp = self.mockUser.post("/create_user/", {"name": "", "password": "1234", "role": "A"}, follow=True)
         self.assertEqual(resp.context["message"], "needs a user name")
 
     def test_UserCreated(self):
-        resp = self.mockUser.post("/createUser/", {"name": "newUser", "password": "abc", "role": "A"}, follow=True)
-        self.assertEqual(resp.context["message"], "Created new user")
+        resp = self.mockUser.post("/create_user/", {"name": "newUser", "password": "abc", "role": "A"}, follow=True)
+        self.assertEqual(resp.context["message"], "SUCCESS! User added successfully.")
         # might want to check that the user has been added to the database as well
 
     def test_badPassword(self):
