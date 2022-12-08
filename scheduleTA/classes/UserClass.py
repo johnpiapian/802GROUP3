bdf93108651d5c5295ab2de2099a58a4750ca17e
johnpiapian <johnpiapian@gmail.com>
2022-12-07 20:48:50 -0600

    Refactor and add getAllUsers()

--- scheduleTA/classes/UserClass.py
+++ scheduleTA/classes/UserClass.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from app.models import User, Course, Section
 
+
 class UserClass:
 
     def __int__(self):
@@ -13,16 +14,13 @@ class UserClass:
     # given a name check if there exists an account
     # associated with the name in the database
     def userExists(self, userName) -> bool:
-        result_from_database = None
         # noinspection PyBroadException
         try:
-            result_from_database = UserClass.getUser(self, userName.upper())
+            return UserClass.getUser(self, userName) is not None
         except:
             return False
-        if result_from_database is None:
-            return False
-        return True
 
+    # TODO: need to revisit
     def passwordCorrect(self, userObject, password):
         try:
             return userObject.password == str(password)
@@ -30,41 +28,52 @@ class UserClass:
             return False
 
     def getRole(self, userName):
-        result_from_database = None
+        # for insensitivity
+        userName = userName.upper()
+
         # noinspection PyBroadException
         try:
-            result_from_database = User.objects.filter(name=userName).values_list('role',flat=True)[0]
+            return User.objects.filter(name=userName).values_list('role', flat=True)[0]
         except:
             return None
-        return result_from_database
+
+    def getAllUsers(self) -> list:
+        try:
+            return User.objects.all()
+        except:
+            return None
 
     # given a valid name return the associated account
     # note: should only return non-sensitive information
     def getUser(self, userName) -> object:
-        result_from_database = None
+        # for insensitivity
+        userName = userName.upper()
+
         # noinspection PyBroadException
         try:
-            result_from_database = User.objects.get(name=userName.upper())
+            return User.objects.get(name=userName)
         except:
             return None
-        return result_from_database
 
     # given user object store it in the database
     def addUser(self, userObj) -> bool:
-        if UserClass.userExists(self,userObj.name):
-            return False
+        # for insensitivity
+        userObj.name = userObj.name.upper()
+
         # noinspection PyBroadException
         try:
+            if UserClass.userExists(self, userObj.name):
+                return False
             userObj.save()
+            return True
         except:
             return False
-        return True
 
+    # TODO: need to revisit
     # given user object update the associated account
     # user object must contain name to find which record to update
     def updateUser(self, userObj) -> bool:
-
-        if UserClass.userExists(self,userObj.name) and len(userObj.password)>1 and len(userObj.role) == 1:
+        if UserClass.userExists(self, userObj.name) and len(userObj.password) > 1 and len(userObj.role) == 1:
             try:
                 userObj.save()
                 return True
@@ -74,10 +83,13 @@ class UserClass:
 
     # given a valid name delete the associated account
     def deleteUser(self, userName) -> bool:
-        if UserClass.getUser(self, userName)!= None:
-            try:
+        # for insensitivity
+        userName = userName.upper()
+        try:
+            if UserClass.userExists(userName):
                 User.objects.filter(name=userName.upper()).delete()
-            except:
+                return True
+            else:
                 return False
-            return True
-        return False
+        except:
+            return False
